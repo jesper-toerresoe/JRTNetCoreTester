2d27fb4a4d24ab43c2cc4241766a1a7b77f97e74
Jesper Rosholm Tørresø <jrt@ase.au.dk>
2018-05-08 15:25:09 +0200

    Første udgave efter følgende tutorial https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/?view=aspnetcore-2.1

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Å]+[a-zA-Å""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Å]+[a-åA-Å""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Rating { get; set; }
+
+
+    }
+}
